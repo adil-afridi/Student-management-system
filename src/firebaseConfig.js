c9752b199166f2293c6fe7df6a7f991106b894e8
adil afridi <adiljamal002@gmail.com>
2024-07-28 14:11:33 +0500

    last commit

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBFbG9IQwhQ3F7O7AquGi_47aW6iM_1w48",
-  authDomain: "student-management-syste-3020d.firebaseapp.com",
-  projectId: "student-management-syste-3020d",
-  storageBucket: "student-management-syste-3020d.appspot.com",
-  messagingSenderId: "729613896236",
-  appId: "1:729613896236:web:dafdad9387112cd8c49f89",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: "G-WEGDDTZWPJ"
 };
 
